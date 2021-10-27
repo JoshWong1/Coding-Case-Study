182a83fd8ddef25f9d25a7cdaa3201dec78f492d
JoshWong1 <wongj09@gmail.com>
2021-10-26 23:09:49 -0400

    added files

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 app.get("/", function (req, res) {
   res.render("index.html");
