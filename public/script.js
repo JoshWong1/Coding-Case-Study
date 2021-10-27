905e6f85e010cc2d2a61f6415c3f0ba8c7a16ac2
JoshWong1 <wongj09@gmail.com>
2021-10-27 11:12:57 -0400

    added files

--- public/script.js
+++ public/script.js
@@ -48,7 +48,11 @@ function load() {
   queryParams.set("pg", id);
   history.replaceState(null, null, "?" + queryParams.toString());
   // Open a new connection, using the GET request on the URL endpoint
-  request.open("GET", "https://xkcd.com/" + num + "info.0.json", true);
+  request.open(
+    "GET",
+    "http://localhost:8080/https://xkcd.com/" + num + "info.0.json",
+    true
+  );
 
   request.onload = function () {
     if (request.status < 200 || request.status >= 400) {
